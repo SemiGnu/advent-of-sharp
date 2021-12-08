b97463b6d70a5a365e5b039569cc88b2c13d8f4b
Torstein Winjum <torstein.winjum@stormgeo.com>
2021-12-08 10:09:45 +0100

    wip 06

--- 06/06.fs
+++ 06/06.fs
@@ -2,13 +2,12 @@
 
 // PARSING 
 
-let lines = System.IO.File.ReadLines("06/test2.txt") |> Seq.toList
+let lines = System.IO.File.ReadLines("06/test2.txt") 
 
-let fish = (List.head lines).Split "," |> Array.map int |> Array.toList 
-
+let fish = (Seq.head lines).Split "," |> Array.map int |> Array.toSeq
 // PART 1
 
-let rec endFish n fish = 
+let rec endFish n (fish:seq<int>) = 
     match n with 
     | 0 -> fish
     | _ -> endFish (n - 1) <| Seq.fold (fun fs f -> if f = 0 then 8::6::fs else (f - 1)::fs) [] fish
@@ -24,18 +23,30 @@ let part1 = sprintf "%d" <| numEndFish 80
 //    | 0 when n > 0 -> 1 + (pown 2 (n/7)) + getFish 8 n
 //    | 0 -> 0
 //    | _ -> getFish (age-1) (n-1) 
-let rec getFish age n =
-    match n with 
-    | 0 -> 1
-    | nn when age = 0 -> (pown 2 ((nn)/7)-1) + getFish 8 (nn-1) 
-    | nn -> getFish (age-1) (nn-1) 
+//let rec getFish age n =
+//    match n with 
+//    | 0 -> 1
+//    | nn when age = 0 -> (pown 2 ((nn)/7)-1) + getFish 8 (nn-1) 
+//    | nn -> getFish (age-1) (nn-1) 
     
-let numEndFish2 n = getFish 3 n
+//let numEndFish2 n = getFish 3 n
         
-let test1  = {1..30} |> Seq.toList |> List.map numEndFish
-let test2  = {1..30} |> Seq.toList |> List.map numEndFish2
+//let test1  = {1..30} |> Seq.toList |> List.map numEndFish
+//let test2  = {1..30} |> Seq.toList |> List.map numEndFish2
 
-let hm = List.zip test1 test2
-            
+//let hm = List.zip test1 test2
+
+let ageFish = Seq.map ((-) 1)
+let resetFish = Seq.filter ((>=) 0) 
+let numNewFish = Seq.length << ( Seq.filter ((<) 0) )
+let newFish fish = Seq.append (resetFish ( ageFish fish ) )  ( Seq.concat <| Seq.init (numNewFish fish) (fun _ -> (List.toSeq [6;8])) )
+
+let rec endFish2 n fish = 
+    match n with 
+    | 0 -> fish
+    | _ -> endFish2 (n-1) <| newFish fish
+
+     
+let numEndFish2 n = Seq.length <| endFish n fish       
     
-let part2 = sprintf "%d" <| numEndFish2 80
+let part2 = sprintf "%d" <| numEndFish2 200
