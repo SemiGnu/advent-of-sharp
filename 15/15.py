8b147e03f1dbbac6e76347c38d6c78fbaaffcac3
Torstein Winjum <twinjum@hotmail.com>
2022-11-18 22:52:50 +0100

    15 done

--- 15/15.py
+++ 15/15.py
@@ -1,4 +1,7 @@
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 from heapq import heappop, heappush
 
 def line_to_array(line) :
@@ -7,16 +10,27 @@ def line_to_array(line) :
         nums.append(int(c))
     return nums
 
+def line_to_array2(line, i) :
+    nums = []
+    for j in range(5):
+        for c in line:
+            n = (int(c) + i + j - 1) % 9 + 1
+            nums.append(int(n))
+    return nums
+
+def lines_to_array2(ls):
+    lines = []
+    for i in range(5):
+        lines += list(map(lambda l: line_to_array2(l, i), ls))
+    return lines
+
 f = open("15/15.txt", "r")
 lines = f.read().split("\n")
-array = list(map(line_to_array, lines))
+array = np.array(list(map(line_to_array, lines)))
+array = np.array(lines_to_array2(lines))
 
 w = len(array[0])
 h = len(array)
-
-draw_array = list(map(lambda y: list(map(lambda x: [25*x, 25*(10-x), 50], y)),array))
-plt.imshow(draw_array, interpolation='nearest')
-plt.show()
 
 def adjacent(cell):
     (y, x) = cell
@@ -26,43 +40,57 @@ def adjacent(cell):
         (y, x+1),
         (y+1, x),
         ]
-    return list(filter(lambda c: c not in visited and 0 <= c[0] < w and 0 <= c[1] < h, adj))
+    return list(filter(lambda c: 0 <= c[0] < w and 0 <= c[1] < h, adj))
+    # return list(filter(lambda c: c not in visited and 0 <= c[0] < w and 0 <= c[1] < h, adj))
     
-def guess(x,y):
+def guess(cell):
+    (y, x) = cell
     return (w - 1 - x) + (h - 1 - y)
 
-open = []
-heappush(open, (0, (0,0)))
+open_set = []
+heappush(open_set, (0, (0,0)))
 
-visited = {(0,0): (0,0)}
+came_from = {}
 
-while len(open) > 0:
-    current = heappop(open)
-    if current[1] == (w-1, h-1):
+g_scores = {}
+f_scores = {}
+for y in range(len(array)):
+    for x in range(len(array[0])):
+        g_scores[y,x] = np.Inf
+        f_scores[y,x] = np.Inf
+g_scores[0,0] = 0
+f_scores[0,0] = guess((0,0))
+
+
+while len(open_set) > 0:
+    (travelled,current) = heappop(open_set)
+    if current == (h-1, w-1):
         break
-    for adj in adjacent(current[1]):
-        (x,y) = adj
-        g = array[y][x] + guess(x,y)
-        heappush(open, (g, adj))
-        visited[adj] = current[1]
 
-path = [(w-1, h-1)]
+    for adj in adjacent(current):
+        tentative = g_scores[current] + array[adj]
+        if tentative < g_scores[adj]:
+            came_from[adj] = current
+            g_scores[adj] = tentative
+            f_scores[adj] = tentative + guess(adj)
+            heappush(open_set, (f_scores[adj], adj))
+
+array[0,0] = 0
+path = [(h-1, w-1)]
 while path[-1] != (0,0):
-    path.append(visited[path[-1]])
+    path.append(came_from[path[-1]])
 
-path = list(reversed(path))
+# path = list(reversed(path))
 
 path_value = 0
 
 for p in path:
-    path_value += array[p[1]][p[0]]
-    array[p[1]][p[0]] = '#'
+    path_value += array[p]
 
-for y in range(h):
-    s = ''
-    for x in range(w):
-        s += str(array[y][x])
-    print(s)
-
+print(array)
 print(path_value)
 
+cmap1 = LinearSegmentedColormap.from_list("gyr", ["lime","yellow","red"])
+plt.imshow(array,cmap=cmap1)
+plt.plot(*np.array(list(map(lambda x: list(reversed(x)),path))).T)
+plt.show()
