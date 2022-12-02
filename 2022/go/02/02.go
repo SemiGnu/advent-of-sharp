c5cc086a86bce7270eb486d70af421be1e7d99ae
Torstein Winjum <torstein.winjum@stormgeo.com>
2022-12-02 12:57:53 +0100

    02 go

---
+++ 2022/go/02/02.go
@@ -0,0 +1,32 @@
+package day02
+
+import (
+	"fmt"
+
+	"github.com/samber/lo"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func scoreLine(line string) int {
+	f, s := line[0]-'A', line[2]-'X'
+	return int(((s-f+4)%3)*3 + 1 + s)
+}
+
+func scoreLine2(line string) int {
+	f, s := line[0]-'A', line[2]-'X'
+	return int(((f + s + 2) % 3) + 1 + s*3)
+}
+
+func Part1(lines []string) {
+	score := lo.SumBy(lines, scoreLine)
+	fmt.Println(score)
+}
+
+func Part2(lines []string) {
+	score := lo.SumBy(lines, scoreLine2)
+	fmt.Println(score)
+}
--- 2022/go/advent.go
+++ 2022/go/advent.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
-	day "github.com/semignu/advent-of-sharp/01"
+	day "github.com/semignu/advent-of-sharp/02"
 )
 
 func check(e error) {
@@ -14,10 +15,18 @@ func check(e error) {
 }
 
 func main() {
-	dat, err := os.ReadFile("01/test.txt")
+	d := "02"
+	test := 0
+	fileName := fmt.Sprintf("%s/input.txt", d)
+	if test == 1 {
+		fileName = fmt.Sprintf("%s/test.txt", d)
+	}
+	dat, err := os.ReadFile(fileName)
 	check(err)
 	file := string(dat)
 	lines := strings.Split(file, "\r\n")
+	fmt.Println("Day", d, "\r\n------\r\nPart 1:")
 	day.Part1(lines)
+	fmt.Println("\r\nPart 2:")
 	day.Part2(lines)
 }
