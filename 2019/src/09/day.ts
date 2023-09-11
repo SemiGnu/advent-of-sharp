e79462b9b0320093f405bea399a7b443246271de
Torstein Winjum <torstein@savvy.no>
2023-09-11 15:40:19 +0200

    09 bad

--- 2019/src/09/day.ts
+++ 2019/src/09/day.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { Runtime } from '../intcode/intcode';
 
 function getData(test: boolean): string[] {
     const file = __filename.replace('day.ts', `${test ? 'test' : 'data'}.txt`);
@@ -7,8 +8,13 @@ function getData(test: boolean): string[] {
 
 
 export function part1(test: boolean): string {
-
-    return '';
+    let program = getData(false).map(n => parseInt(n));
+    // program = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+    // program = [1102,34915192,34915192,7,4,7,99,0];
+    const rt = new Runtime(program);
+    let output = rt.io(1);
+    console.log(rt)
+    return '' + output;
 }
 
 
--- 2019/src/intcode/intcode.ts
+++ 2019/src/intcode/intcode.ts
@@ -5,11 +5,13 @@ export class Runtime {
     outputs: number[] = [];
     pointer: number = 0;
     realtiveBase: number = 0;
+    operations: number = 0;
     halted: boolean = false;
 
     constructor(program: number[]) {
         this.originalProgram = program;
         this.program = [...this.originalProgram];
+        if (this.program.some(n => n > 9007199254740991)) throw new Error("FUCK");
     };
 
     io(...inputs: number[]): number[] {
@@ -17,6 +19,9 @@ export class Runtime {
         this.outputs = [];
         this.program = [...this.originalProgram];
         this.pointer = 0;
+        this.halted = false;
+        this.realtiveBase = 0;
+        this.operations = 0;
         this.run();
         return this.outputs;
     }
@@ -28,54 +33,91 @@ export class Runtime {
     }
 
     run() {
+        let i = 0;
         while (this.execute());
     }
 
     getValue(mode: string): number {
-        return mode === '1' 
-            ? this.program[this.pointer++] 
-            : this.program[this.program[this.pointer++]];
+        let value = 0;
+        switch (mode) {
+            case '0':
+                value = this.program[this.program[this.pointer++]] ?? 0;
+                break;
+            case '1':
+                value = this.program[this.pointer++] ?? 0;
+                break;
+            case '2':
+                value = this.program[this.realtiveBase + this.program[this.pointer++]] ?? 0;
+                break;
+            default:
+                throw new Error(`Halted on mode '${mode}'`);
+        }
+        console.log(value)
+        return value;
+    }
+
+    setValue(value: number) {
+        if (value > 9007199254740991) throw new Error("FUCK");
+        console.log(`Storing ${value} at:`)
+        const location = this.getValue('1')
+        if (this.program.length <= location) {
+            this.program = [...this.program, ...[...Array(location - this.program.length + 1)].map(_ => 0)]
+        }
+        this.program[location] = value;
     }
     
     execute(): boolean {
         const modes = ('' + this.program[this.pointer++]).padStart(5, '0');
         const instruction = modes.slice(3);
+        this.operations++;
         switch (instruction) {
             case '01': // add
+                console.log("add")
                 const sum = this.getValue(modes[2]) + this.getValue(modes[1]);
-                this.program[this.program[this.pointer++]] = sum;
+                this.setValue(sum);
                 return true;
             case '02': // multiply
+                console.log("multiply")
                 const product = this.getValue(modes[2]) * this.getValue(modes[1]);
-                this.program[this.program[this.pointer++]] = product;
+                this.setValue(product);
                 return true;
             case '03': // input
                 if(!this.inputs.length) {
                     this.pointer--;
                     return false;
                 }
-                this.program[this.program[this.pointer++]] = this.inputs.pop() ?? 0;
+                console.log("input")
+                this.setValue(this.inputs.pop() ?? 0);
                 return true;
             case '04': // output
+                console.log("output")
                 this.outputs.push(this.getValue(modes[2]));
                 return true;
             case '05': // jump-if-true
+                console.log("jump-if-true")
                 const isTrue = this.getValue(modes[2]) != 0;
                 this.pointer = isTrue ? this.getValue(modes[1]) : this.pointer + 1;
                 return true;
             case '06': // jump-if-false
+                console.log("jump-if-false")
                 const isFalse = this.getValue(modes[2]) == 0;
                 this.pointer = isFalse ? this.getValue(modes[1]) : this.pointer + 1;
                 return true;
             case '07': // less than
+                console.log("less than")
                 const lt = this.getValue(modes[2]) < this.getValue(modes[1]);
-                this.program[this.program[this.pointer++]] = lt ? 1 : 0;
+                this.setValue(lt ? 1 : 0);
                 return true;
             case '08': // equals
+                console.log("equals")
                 const eq = this.getValue(modes[2]) == this.getValue(modes[1]);
-                this.program[this.program[this.pointer++]] = eq ? 1 : 0;
+                this.setValue(eq ? 1 : 0);
                 return true;
-            case '99':
+            case '09': // adjust relavite base
+                console.log("adjust relavite base")
+                this.realtiveBase += this.getValue(modes[2]);
+                return true;
+            case '99': // halt
                 this.halted = true;
                 return false;
             default:
