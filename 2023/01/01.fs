97a8350cf57b0bd45f8f6f91d1fc533bcdd43180
Torstein Winjum <torstein.winjum@instech.no>
2023-12-01 13:22:17 +0100

    embetter

--- 2023/01/01.fs
+++ 2023/01/01.fs
@@ -6,7 +6,6 @@ open System
 // let lines = Helpers.getTest 1 (Some 3)
 let lines = Helpers.getData 1
 
-
 let toNumber s = (Seq.find Char.IsDigit s |> int |> (+) -48 ) * 10 + (Seq.rev s |> Seq.find Char.IsDigit |> int |> (+) -48)
 let part1 () = Seq.map toNumber lines |> Seq.sum |> printfn "%i"
 
@@ -14,12 +13,11 @@ let realNumbers = seq {1..9} |> Seq.map string
 let stringNumbers = seq ["one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"]
 let allNumbers = Seq.concat [ realNumbers; stringNumbers ]
 let hmm = (<=) 0
-let getIndices (line: string) = Seq.map (fun (n: string) -> (line.IndexOf n, line.LastIndexOf n)) allNumbers
-                                |> Seq.mapi (fun i x -> (x, i % 9 + 1))
-                                |> Seq.filter (fst >> fst >> (<=) 0)
-let toNumber2 indices = (Seq.minBy (fst>>fst) indices |> snd) * 10
-                        + (Seq.maxBy (fst>>snd) indices |> snd)
-let part2 () = Seq.map getIndices lines
-               |> Seq.map toNumber2
+
+let toDigit (number: string) = Seq.findIndex number.Equals allNumbers |> fun n -> n % 9 + 1
+let firstNumber (line: string) = Seq.minBy (fun (n: string) -> line.IndexOf n |> uint) allNumbers |> toDigit
+let lastNumber (line: string) = Seq.maxBy (fun (n: string) -> line.LastIndexOf n) allNumbers |> toDigit
+let toNumber2 line = (firstNumber line) * 10 + lastNumber line
+let part2 () = Seq.map toNumber2 lines
                |> Seq.sum
                |> printfn "%i"
