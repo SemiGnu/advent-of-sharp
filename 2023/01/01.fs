b7bf8bdeed74db24a0235998d707b02713cd695c
Torstein Winjum <torstein.winjum@instech.no>
2023-12-01 13:58:21 +0100

    embest

--- 2023/01/01.fs
+++ 2023/01/01.fs
@@ -1,23 +1,17 @@
 ﻿module AoC2023.Day01
 
-open AoC2023
 open System
 
-// let lines = Helpers.getTest 1 (Some 3)
-let lines = Helpers.getData 1
+let lines = System.IO.File.ReadLines "01/data.txt"
 
-let toNumber s = (Seq.find Char.IsDigit s |> int |> (+) -48 ) * 10 + (Seq.rev s |> Seq.find Char.IsDigit |> int |> (+) -48)
+let toNumber s = (Seq.find Char.IsDigit s |> (string >> int) ) * 10 + (Seq.rev s |> Seq.find Char.IsDigit |> (string >> int))
 let part1 () = Seq.map toNumber lines |> Seq.sum |> printfn "%i"
 
-let realNumbers = seq {1..9} |> Seq.map string
+let digitNumbers = seq {1..9} |> Seq.map string
 let stringNumbers = seq ["one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"]
-let allNumbers = Seq.concat [ realNumbers; stringNumbers ]
-let hmm = (<=) 0
-
-let toDigit (number: string) = Seq.findIndex number.Equals allNumbers |> fun n -> n % 9 + 1
-let firstNumber (line: string) = Seq.minBy (fun (n: string) -> line.IndexOf n |> uint) allNumbers |> toDigit
-let lastNumber (line: string) = Seq.maxBy (fun (n: string) -> line.LastIndexOf n) allNumbers |> toDigit
-let toNumber2 line = (firstNumber line) * 10 + lastNumber line
-let part2 () = Seq.map toNumber2 lines
-               |> Seq.sum
-               |> printfn "%i"
+let allNumbers = Seq.concat [ digitNumbers; stringNumbers ]
+let toDigit (number: string) = (Seq.findIndex number.Equals allNumbers) % 9 + 1
+let firstDigit (line: string) = allNumbers |> Seq.minBy (line.IndexOf >> uint) |> toDigit
+let lastDigit (line: string) = allNumbers |> Seq.maxBy line.LastIndexOf |> toDigit
+let toNumber2 line = (firstDigit line) * 10 + lastDigit line
+let part2 () = Seq.map toNumber2 lines |> Seq.sum |> printfn "%i"
