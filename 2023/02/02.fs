a3be273c333e6611a3ba8be256a82bb6dfb9648a
Torstein Winjum <twinjum@hotmail.com>
2023-12-03 15:19:49 +0100

    02.2

--- 2023/02/02.fs
+++ 2023/02/02.fs
@@ -1,15 +1,6 @@
 ﻿module AoC2023.Day02
 
-open System.Text.RegularExpressions
-
 let lines = System.IO.File.ReadLines "02/data"
-
-type Cube =
-    | Red of int
-    | Green of int
-    | Blue of int
-type Draw = Cubes of seq<Cube>
-type Game = Draws of seq<Draw>
 
 let parseCube (cube:string) = match cube.Split(" ") with
                               | [| n; "red" |] -> [|(int n); 0; 0|]
@@ -26,8 +17,7 @@ let parseGame (game:string) = (game.Split(":")[1]).Split(";", System.StringSplit
                               |> Seq.toArray
 
 let possible draw = draw |> Seq.fold2 (fun s m d -> s && m >= d) true [|12;13;14|]
-
 let part1 () = lines |> Seq.map parseGame  |> Seq.mapi (fun i game -> if (Seq.forall possible game) then i + 1 else 0) |> Seq.sum |> printfn "%i"
 
-
-let part2 () = lines |> Seq.iter (printfn "%s")
+let minPossible =  Seq.fold (Seq.map2 max) [|0;0;0|]
+let part2 () = lines |> Seq.sumBy (parseGame >> minPossible >> Seq.fold (*) 1) |> printfn "%i"
