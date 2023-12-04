a9f57cddd1c1c41e6839b889564b32e09c044c66
Torstein Winjum <torstein.winjum@instech.no>
2023-12-04 13:41:02 +0100

    04.1

--- 2023/00/00.fs
+++ 2023/00/00.fs
@@ -1,6 +1,6 @@
 ﻿module AoC2023.Day00
 
-let lines = System.IO.File.ReadLines "00/data.txt"
+let lines = System.IO.File.ReadLines "00/test"
 
 let part1 () = lines |> Seq.iter (printfn "%s")
 let part2 () = lines |> Seq.iter (printfn "%s")
---
+++ 2023/04/04.fs
@@ -0,0 +1,11 @@
+﻿module AoC2023.Day04
+
+let lines = System.IO.File.ReadLines "04/test"
+
+// 13
+
+let parseCard (line:string) = (line.Split(":")[1]).Split("|") |> Seq.map (fun (s:string) -> s.Split(" ", System.StringSplitOptions.RemoveEmptyEntries) |> Seq.map int |> Set.ofSeq)
+let scoreCard = Set.intersectMany >> Set.count >> fun n -> pown 2 (n - 1)
+
+let part1 () = lines |> Seq.sumBy (parseCard >> scoreCard) |> printfn "%i"
+let part2 () = lines |> Seq.iter (printfn "%s")
