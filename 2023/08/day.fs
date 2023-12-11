b22da7c0cf34e48cc3b3049549ce256450881e66
Torstein Winjum <torstein.winjum@instech.no>
2023-12-11 17:42:17 +0100

    08.2 derp

--- 2023/08/day.fs
+++ 2023/08/day.fs
@@ -22,14 +22,37 @@ let rec spookyTraverse found step (instructions:string) map (loc:string) =
     | 'L' -> spookyTraverse found (step + 1) instructions map (Map.find loc map |> fst)
     | 'R' -> spookyTraverse found (step + 1) instructions map (Map.find loc map |> snd)
 
-let initSteps = startNodes |> Array.map (spookyTraverse true 0 instructions map)
-let loopSteps = startNodes |> Array.map (spookyTraverse false 0 instructions map)
+let initSteps = startNodes |> Array.map (spookyTraverse true 0 instructions map >> int64)
+let loopSteps = startNodes |> Array.map (spookyTraverse false 0 instructions map >> int64)
 
-let rec find (steps:int[]) (i:int) =
-    if steps |> Array.forall ((<>) -1) then steps |> Array.min
-    else find (steps |> Array.map (fun s -> if s = -1 then -1 else s + i)) (i + 1)
+let indexOfLowest (steps: int64 array) = Array.findIndex ((=) (Array.min steps)) steps
+let lowArray (steps: int64 array) =
+    let min = indexOfLowest steps
+    loopSteps |> Array.mapi (fun i s -> if i = min then s else 0)
 
-let part2 () =  1 |> printfn "%A"
+let rec incrementLowest (steps: int64 array) =
+    if steps |> Array.forall ((=) steps[0])
+    then steps[0]
+    else if steps |> Array.forall ((=) 0)
+    then 0
+    else steps |> lowArray |> Array.map2 (+) steps |> incrementLowest
+
+let hm n = (((*) n) >> (+) )
+
+
+let rec intersect max lcm n =
+    // if  Array.map2 (fun i s -> s + n * loopSteps[i]) |> Array.forall ((%) lcm >> (=) 0L)
+    if  Array.map2 (((*) n) >> (+) ) loopSteps initSteps |> Array.forall (fun s -> s % lcm = 0L)
+    then n * max
+    else intersect max lcm (n + 1L)
+
+let part2 () =  intersect 19512L 6009696L 1L |> printfn "%A"
+// let part2 () =  loopSteps |> printfn "%A"
 
 // 21409; 16531; 19241; 19783; 14363; 15989
 // 19512; 2439; 2981; 1897; 11924; 8672
+// 6009696
+// 19512 2439 2981 1897 11924 8672 21409 16531 19241 19783 14363 15989
+// 469373449998698816
+// 153934641126
+// 9223372036854775807
